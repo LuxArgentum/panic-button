fbe0c54c6f9d43d9f92f9cbaecf95018fda56497
MatthewSong <matthewsong18@gmail.com>
2024-07-15 23:56:31 -0400

    Fix Panic Button not building

--- src/main/java/com/lux/panicbutton/PanicButton.java
+++ src/main/java/com/lux/panicbutton/PanicButton.java
@@ -1,7 +1,12 @@
 package com.lux.panicbutton;
 
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.player.PlayerEntity;
 
-public class PanicButton {
+public class PanicButton implements ModInitializer {
 
+    @Override
+    public void onInitialize() {
+
+    }
 }
